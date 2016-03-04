fbc8d328cb8a06906203bdd46e34c00b33d45c62
Andreas Streichardt <andreas@arangodb.com>
2016-03-04 14:49:27 +0100

    Properly check that arangodb is running

--- helper.bash
+++ helper.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 deploy_arangodb() {
   cat arangodb.json | sed -e "s/{{ IP }}/$CURRENT_IP/g" | curl -X POST "$CURRENT_IP":8080/v2/apps -d @- --dump - -H "Content-Type: application/json" && echo
@@ -13,5 +14,7 @@ deploy_arangodb() {
     STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$MGMT_URL"/v1/health.json || true)
     sleep 1
   done
-  
+  COORDINATOR=$(curl "$MGMT_URL"/v1/endpoints.json | jq '.coordinators[0]')
+  [[ (-n "$COORDINATOR") && ("$COORDINATOR" != "null") ]] || (echo "No coordinator present :S" && exit 1)
+  [[ "$(curl $COORDINATOR/_api/collection | jq '.collections | length > 0')" = "true" ]] || (echo "No collections on coordinator. Cluster bootstrap must be broken" && exit 1)
 }
